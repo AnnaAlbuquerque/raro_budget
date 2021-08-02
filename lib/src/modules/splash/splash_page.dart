852a259de3e1ecc4d0d6dafb6aacd57e22235435
Andre Kubotsu <contato201@gmail.com>
2021-08-02 18:32:26 -0300

    new images and animation

--- lib/src/modules/splash/splash_page.dart
+++ lib/src/modules/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:raro_budget/src/modules/home/home_page.dart';
 import 'package:raro_budget/src/shared/constants/app_colors.dart';
@@ -12,29 +13,102 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((timeStamp) {
-      Future.delayed(Duration(seconds: 2)).then(
-        (value) => Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (BuildContext context) => HomePage(),
-          ),
-        ),
-      );
-    });
+    // WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((timeStamp) {
+    //   Future.delayed(Duration(seconds: 2)).then(
+    //     (value) => Navigator.of(context).push(
+    //       MaterialPageRoute(
+    //         builder: (BuildContext context) => HomePage(),
+    //       ),
+    //     ),
+    //   );
+    // });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: AppColors.cyanToPurple,
-        ),
-        child: Stack(
-          children: [],
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: AppColors.cyanToPurple,
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              AnimatedCard(
+                direction: AnimatedCardDirection.top,
+                duration: Duration(milliseconds: 700),
+                curve: Curves.bounceOut,
+                child: Align(
+                  alignment: Alignment(0, -0.22),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                          image:
+                              AssetImage('assets/images/budget_icon_only.png')),
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.left,
+                duration: Duration(milliseconds: 700),
+                child: Align(
+                  alignment: Alignment(0, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                          image: AssetImage(
+                              'assets/images/budget_logo_text_only.png')),
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.right,
+                duration: Duration(milliseconds: 700),
+                child: Align(
+                  alignment: Alignment(0, 0.15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                          image: AssetImage(
+                              'assets/images/budget_subtitle_only.png')),
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.bottom,
+                duration: Duration(milliseconds: 700),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                            image: AssetImage(
+                                'assets/images/powered_by_raro.png')),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
