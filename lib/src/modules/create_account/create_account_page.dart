ebe63800a6297bb2c821e9dc4361784674e6adeb
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2021-08-05 19:46:11 -0300

    Adds page create account

---
+++ lib/src/modules/create_account/create_account_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:raro_budget/src/modules/create_account/create_account_controller.dart';
+// import 'package:raro_budget/src/modules/create_account/widgets/name_email_widget.dart';
+import 'package:raro_budget/src/modules/create_account/widgets/phone_cpf_widget.dart';
+import 'package:raro_budget/src/shared/widgets/custom_button/custom_button_widget.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  CreateAccountPage({Key? key}) : super(key: key);
+
+  @override
+  _CreateAccountPageState createState() => _CreateAccountPageState();
+}
+
+class _CreateAccountPageState extends State<CreateAccountPage> {
+  //final CreateAccountController _controller = CreateAccountController();
+  final _pageViewController = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            flex: 1,
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              controller: _pageViewController,
+              children: [
+                //NameEmailWidget(),
+                PhoneCPFWidget(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomButton(
+                iconLeft: Icons.arrow_back,
+                text: 'VOLTAR',
+                useGradientBackground: false,
+                onTap: () {
+                  // _controller.pageNumberDecrement();
+                  _pageViewController.previousPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.linear,
+                  );
+                },
+              ),
+              // Observer(builder: (_) {
+              //   return Text("${_controller.pageNumber}/4");
+              // }),
+              CustomButton(
+                iconRight: Icons.forward,
+                text: 'CONTINUAR',
+                useGradientBackground: true,
+                onTap: () {
+                  _pageViewController.nextPage(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.linear,
+                  );
+                  // _controller.pageNumberIncrement();
+
+                  // if (_formKey.currentState!.validate()) {
+                  //   showDialog(
+                  //     context: context,
+                  //     builder: (context) => Column(
+                  //       mainAxisAlignment: MainAxisAlignment.start,
+                  //       children: [
+                  //         Card(
+                  //           color: Colors.green,
+                  //           shadowColor: Color(0xFFCCCCCC),
+                  //           child: Padding(
+                  //             padding: const EdgeInsets.all(16.0),
+                  //             child: Text(
+                  //               "Para testar a validação! Neste caso, passou!",
+                  //               style: TextStyle(
+                  //                   fontSize: 24, color: Colors.white),
+                  //             ),
+                  //           ),
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   );
+                  // }
+                },
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          )
+        ],
+      ),
+    );
+  }
+}
