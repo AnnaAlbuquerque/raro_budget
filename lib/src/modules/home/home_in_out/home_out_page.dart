07dab29bc82f27030caf5a86d8744223a24de8ca
Deivison Silva <deivisondamiao01@outlook.com>
2021-08-12 16:50:05 -0300

    creates custom dropdownform widget and adds hims in home out page

--- lib/src/modules/home/home_in_out/home_out_page.dart
+++ lib/src/modules/home/home_in_out/home_out_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../../../../src/shared/constants/app_colors.dart';
-import '../../../../src/shared/constants/app_text_styles.dart';
-import '../../../../src/shared/widgets/custom_appbar/custom_appbar.dart';
-import '../../../../src/shared/widgets/custom_button_logged/custom_button_logged_widget.dart';
-import '../../../../src/shared/widgets/custom_text_form_field/custom_text_form_field_widget.dart';
+import '../../../shared/constants/app_colors.dart';
+import '../../../shared/constants/app_text_styles.dart';
+import '../../../shared/widgets/custom_appbar/custom_appbar.dart';
+import '../../../shared/widgets/custom_button_logged/custom_button_logged_widget.dart';
+import '../../../shared/widgets/custom_text_form_field/custom_text_form_field_widget.dart';
+import './widgets/custom_dropdownbuttonform_widget.dart';
 
 class HomeOutPage extends StatefulWidget {
   const HomeOutPage({Key? key}) : super(key: key);
@@ -13,20 +14,9 @@ class HomeOutPage extends StatefulWidget {
   _HomeOutPageState createState() => _HomeOutPageState();
 }
 
-class DropdownMenuItemData {
-  final String category;
-  final String icon;
-  final Color color;
-
-  DropdownMenuItemData({
-    required this.category,
-    required this.icon,
-    required this.color,
-  });
-}
-
 class _HomeOutPageState extends State<HomeOutPage> {
   TextEditingController _valueController = TextEditingController();
+  String date = DateFormat('dd/MM/yyyy').format(DateTime.now());
   DropdownMenuItemData? item;
 
   List<DropdownMenuItemData> items = [
@@ -76,83 +66,105 @@ class _HomeOutPageState extends State<HomeOutPage> {
         title: 'Saída',
         prefSize: 120.0,
       ),
-      body: Card(
-        margin: const EdgeInsets.only(
+      body: Padding(
+        padding: const EdgeInsets.only(
             left: 16.0, top: 16.0, right: 16.0, bottom: 40.0),
-        child: Container(
-          margin: const EdgeInsets.only(
-              left: 54.0, top: 88.0, right: 54.0, bottom: 115.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomTextFormField(
-                name: 'Valor em R\$',
-                textInputType: TextInputType.number,
-                controller: _valueController,
-              ),
-              const SizedBox(height: 32.0),
-              DropdownButtonFormField<DropdownMenuItemData>(
-                decoration: InputDecoration(
-                  labelText: 'Tipo de saída',
-                  // hintText: 'Escolha',
+        child: Card(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.circular(7.0),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.black.withOpacity(0.12),
+                  offset: Offset(0.0, 1.0),
+                  blurRadius: 10.0,
                 ),
-                value: item,
-                onChanged: (value) {
-                  setState(() {
-                    item = value as DropdownMenuItemData;
-                  });
-                },
-                selectedItemBuilder: (BuildContext context) {
-                  return items.map<Widget>((DropdownMenuItemData item) {
-                    return Row(
-                      children: [
-                        Text(item.category),
-                      ],
-                    );
-                  }).toList();
-                },
-                items: items.map((DropdownMenuItemData item) {
-                  return DropdownMenuItem<DropdownMenuItemData>(
-                    value: item,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 24.0,
-                          height: 24.0,
-                          decoration: BoxDecoration(
-                            color: item.color,
-                            shape: BoxShape.circle,
+                BoxShadow(
+                  color: AppColors.black.withOpacity(0.14),
+                  offset: Offset(0.0, 4.0),
+                  blurRadius: 5.0,
+                ),
+                BoxShadow(
+                  color: AppColors.black.withOpacity(0.2),
+                  offset: Offset(0.0, 2.0),
+                  blurRadius: 4.0,
+                  spreadRadius: -1.0,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 54.0, top: 88.0, right: 54.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomTextFormField(
+                      name: 'Valor em R\$',
+                      textInputType: TextInputType.number,
+                      controller: _valueController,
+                    ),
+                    const SizedBox(height: 32.0),
+                    CustomDropdownButtonForm(
+                      value: item,
+                      onChanged: (value) {
+                        setState(() {
+                          item = value as DropdownMenuItemData;
+                        });
+                      },
+                      selectedItemBuilder: (BuildContext context) {
+                        return items.map<Widget>((DropdownMenuItemData item) {
+                          return Row(
+                            children: [
+                              Text(item.category),
+                            ],
+                          );
+                        }).toList();
+                      },
+                      items: items.map((DropdownMenuItemData item) {
+                        return DropdownMenuItem<DropdownMenuItemData>(
+                          value: item,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 24.0,
+                                height: 24.0,
+                                decoration: BoxDecoration(
+                                  color: item.color,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image(image: AssetImage(item.icon)),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(item.category),
+                            ],
                           ),
-                          child: Image(image: AssetImage(item.icon)),
-                        ),
-                        const SizedBox(width: 8.0),
-                        Text(item.category),
-                      ],
+                        );
+                      }).toList(),
                     ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 38.0),
-              InkWell(
-                onTap: () {},
-                child: Ink(
-                  child: Container(
-                    child: Text(
-                      DateFormat('dd/MM/yyyy').format(DateTime.now()),
-                      style: TextStyles.purple14w500Roboto,
+                    const SizedBox(height: 38.0),
+                    InkWell(
+                      onTap: () {},
+                      child: Ink(
+                        child: Text(date, style: TextStyles.purple14w500Roboto),
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: CustomButtonLoggedFlow(
-        onTap: () {},
+        useGradientBackground: true,
         text: 'INSERIR',
         useIconAdd: true,
+        onTap: () {},
       ),
     );
   }
