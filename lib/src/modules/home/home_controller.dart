4c2712bdc00f2e0424b41f48022ea4d40bb933aa
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2021-08-17 02:14:03 -0300

    Removes duplicated user model

--- lib/src/modules/home/home_controller.dart
+++ lib/src/modules/home/home_controller.dart
@@ -1,7 +1,13 @@
-import 'package:raro_budget/src/core/entities/user_model.dart';
+import 'package:raro_budget/src/shared/models/user_model.dart';
 
 class HomeController {
-  UserModel user = UserModel(name: "José");
+  UserModel user = UserModel(
+      name: "José",
+      email: "jose@teste.com",
+      phone: "111222333",
+      cpf: "123.456.789.00",
+      terms: true,
+      password: "xxx");
   bool error = false;
   String balance = "3.000";
 }
