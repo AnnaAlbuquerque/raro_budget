2f5fea4fa932e2e4cfd62f972139241902ad8f66
Andre Kubotsu <contato201@gmail.com>
2021-08-20 22:09:37 -0300

    reorganizing widgets and refactoring drawer

--- lib/src/modules/home/home_main/home_page.dart
+++ lib/src/modules/home/home_main/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:raro_budget/src/modules/home/home_main/custom_card_dia_a_dia/custom_card_dia_a_dia.dart';
-import 'package:raro_budget/src/modules/home/home_main/custom_drawer/custom_drawer_widget.dart';
-import 'package:raro_budget/src/modules/home/home_main/custom_general_balance/custom_general_balance_widget.dart';
+
 import 'package:raro_budget/src/modules/home/home_main/home_controller.dart';
+import 'package:raro_budget/src/modules/home/widgets/custom_card_dia_a_dia/custom_card_dia_a_dia.dart';
+import 'package:raro_budget/src/modules/home/widgets/custom_drawer/custom_drawer_widget.dart';
+import 'package:raro_budget/src/modules/home/widgets/custom_general_balance/custom_general_balance_widget.dart';
 
 import 'package:raro_budget/src/modules/home/widgets/custom_last_transactions/custom_last_transactions_widget.dart';
 import 'package:raro_budget/src/shared/constants/app_text_styles.dart';
--- lib/src/modules/login/create_account/create_account_controller.dart
+++ lib/src/modules/login/create_account/create_account_controller.dart
@@ -56,8 +56,8 @@ abstract class CreateAccountBase with Store {
 
     repository.addUser(savedUser);
 
-    print("USER CREATED!");
     print(savedUser);
+    print("USER CREATED!");
   }
 
   @action
